d29ce1729a604012b4fe910aeebf82ed373f41ba
Toma Suciu <tomasuciu@gmail.com>
2025-10-20 22:38:24 -0700

    Start on main method; determine rank and parallelism configuration

--- train.py
+++ train.py
@@ -1,14 +1,54 @@
+import os
+
 import torch
 import torch.distributed as dist
 from torch.distributed.fsdp import (
     FullyShardedDataParallel as FSDP
 )
 import transformers
+from torch.distributed.fsdp.wrap import (
+    transformer_auto_wrap_policy,
+    size_based_auto_wrap_policy,
+)
+from torch.distributed.elastic.multiprocessing.errors import record
+
 
 
 def main():
-    pass
+    if not dist.is_initialized():
+        dist.init_process_group(backend="nccl")
+    
+    local_rank = int(os.environ.get("LOCAL_RANK", 0))
+    global_rank = int(os.environ.get("RANK", 0))
+    world_size = int(os.environ.get("WORLD_SIZE", 1))
+    
+    torch.cuda.set_device(local_rank)
+    
+    # TODO: standardize logging
+    print(f"Rank {global_rank}/{world_size}, Local rank: {local_rank}")
+
+    if global_rank == 0:
+        # TODO: create experiment, if tracking
+        pass
+
+    # select oss weights for fine-tuning
+    model = None
+
+    auto_wrap_policy = size_based_auto_wrap_policy(
+        min_num_params=1e8
+    )
+    
+    model = FSDP(
+        model,
+        auto_wrap_policy=auto_wrap_policy,
+        device_id=torch.cuda.current_device(),
+        limit_all_gathers=True,
+    )
+    
+    return model
+
+
 
 
 if __name__ == "__main__":
-    pass
+    main()
