a0be1119b3b040560bb2ab790d0c0fe1c138d3f6
Toma Suciu <tomasuciu@gmail.com>
2025-10-20 22:32:59 -0700

    Training entrypoint skeleton

---
+++ train.py
@@ -0,0 +1,14 @@
+import torch
+import torch.distributed as dist
+from torch.distributed.fsdp import (
+    FullyShardedDataParallel as FSDP
+)
+import transformers
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
